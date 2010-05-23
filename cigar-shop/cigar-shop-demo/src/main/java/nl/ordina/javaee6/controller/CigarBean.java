e05f7e13752a9d6329a3272107f13ef82b6ae027
Martijn Blankestijn <martijnblankestijn@gmail.com>
2010-05-23 17:24:09 +0200

    lifecycle events toegevoegd

--- cigar-shop/cigar-shop-demo/src/main/java/nl/ordina/javaee6/controller/CigarBean.java
+++ cigar-shop/cigar-shop-demo/src/main/java/nl/ordina/javaee6/controller/CigarBean.java
@@ -3,10 +3,11 @@ package nl.ordina.javaee6.controller;
 import nl.ordina.javaee6.domain.Cigar;
 import nl.ordina.javaee6.service.CigarService;
 
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
 import javax.enterprise.context.RequestScoped;
 import javax.inject.Inject;
 import javax.inject.Named;
-import java.util.Collections;
 import java.util.List;
 
 @RequestScoped
@@ -15,9 +16,22 @@ public class CigarBean {
     @Inject
     private CigarService cigarService;
 
-    public List<Cigar> getCigars() {
-        return cigarService.getCigars();
+    private List<Cigar> cigars;
+
+    @PostConstruct
+    private void init() {
+        cigars = cigarService.getCigars();
     }
 
-    
+    @PreDestroy
+    public void end() {
+        System.out.println("Clean up of managed bean");
+    }
+
+
+    public List<Cigar> getCigars() {
+        return cigars;
+    }
+
+
 }
