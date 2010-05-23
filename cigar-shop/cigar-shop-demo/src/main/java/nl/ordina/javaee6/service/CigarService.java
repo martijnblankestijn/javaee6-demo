a349bdd81297ace22a86b152ced1dbeed9ab6135
Martijn Blankestijn <martijnblankestijn@gmail.com>
2010-05-23 15:12:07 +0200

    adding of metamodel generation for JPA usage of metamodel in service

--- cigar-shop/cigar-shop-demo/src/main/java/nl/ordina/javaee6/service/CigarService.java
+++ cigar-shop/cigar-shop-demo/src/main/java/nl/ordina/javaee6/service/CigarService.java
@@ -1,6 +1,7 @@
 package nl.ordina.javaee6.service;
 
 import nl.ordina.javaee6.domain.Cigar;
+import nl.ordina.javaee6.domain.Cigar_;
 
 import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
@@ -17,13 +18,13 @@ public class CigarService {
 
     public List<Cigar> getCigars() {
         CriteriaBuilder cb = em.getCriteriaBuilder();
+
         CriteriaQuery<Cigar> query = cb.createQuery(Cigar.class);
         Root<Cigar> root = query.from(Cigar.class);
 
-        query.orderBy(cb.asc(root.get("name")));
+        query.orderBy(cb.asc(root.get(Cigar_.name)));
 
-            return em.createQuery(query).getResultList();
-//          return em.createQuery("select c from Cigar c", Cigar.class).getResultList();
+        return em.createQuery(query).getResultList();
     }
 
     public void persist(Cigar cigar) {
